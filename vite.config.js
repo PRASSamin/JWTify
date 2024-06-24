1e95a3d5cd841aebc4b924ee88836fccc3cae79b
PRASSamin <prassamin@gmail.com>
2024-06-24 06:31:40 +0600

    v1.0.4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'generate-redirects',
+      closeBundle() {
+        fs.writeFileSync('dist/_redirects', '/* /index.html 200')
+      }
+    }
+  ],
 })
