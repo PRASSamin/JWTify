6ed44642cd5c1c6d323162519ccef3289fb09c3b
PRASSamin <prassamin@gmail.com>
2024-06-24 06:18:10 +0600

    v1.0.3

--- src/components/decoder.jsx
+++ src/components/decoder.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './global/header'
 import DecoderTool from './jwt/decode-tool'
 import JWTDoc from './jwt/jwt-doc'
 import JWTTop from './jwt/jwt-top'
 const Decoder = () => {
+
+  useEffect(() => {
+    document.title = "JWTify - Decode JWT"
+  })
+
+
   return (
     <div className="">
       <Header/>
--- src/components/encoder.jsx
+++ src/components/encoder.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './global/header'
 import EncoderTool from './jwt/encode-tool'
 import JWTDoc from './jwt/jwt-doc'
 import JWTTop from './jwt/jwt-top'
 const Encoder = () => {
+
+  useEffect(() => {
+    document.title = "JWTify - Encode JWT"
+  })
+
   return (
     <div className="">
       <Header/>
