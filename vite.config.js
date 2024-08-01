05f4faaf3b3fab34d641d78e3ca1504465088b40
PRASSamin <prassamin@gmail.com>
2024-08-01 17:26:34 +0600

    v1.0.15

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
-import ViteStaticCopy from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,21 +12,5 @@ export default defineConfig({
         fs.writeFileSync("dist/_redirects", "/* /index.html 200");
       },
     },
-    ViteStaticCopy({
-      targets: [
-        {
-          src: "./sitemap.xml",
-          dest: "",
-        },
-      ],
-    }),
-    ViteStaticCopy({
-      targets: [
-        {
-          src: "./BingSiteAuth.xml",
-          dest: "",
-        },
-      ],
-    }),
   ],
 });
