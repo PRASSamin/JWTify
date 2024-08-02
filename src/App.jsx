2c8ffe874b6f24b4521a99390b9a8d94b051e281
PRASSamin <prassamin@gmail.com>
2024-08-02 22:47:16 +0600

    v1.0.18

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import Encoder from './components/encoder'
-import Decoder from './components/decoder';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import Decoder from "./components/decoder";
+import Encoder from "./components/encoder";
 
-const App = () => {
+const RedirectWrapper = ({ children }) => {
   const location = useLocation();
 
   useEffect(() => {
     if (
-      location.pathname !== "/jwt-decoder" ||
-      location.pathname !== "/jwt-encoder" ||
+      location.pathname !== "/jwt-decoder" &&
+      location.pathname !== "/jwt-encoder" &&
       location.pathname !== "/home"
     ) {
       window.location.replace("/jwt-decoder");
     }
   }, [location]);
 
+  return children;
+};
+
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <RedirectWrapper>
         <Routes>
-          {["/jwt-decoder", "/home"].map((path, i) => (
-            <Route
-              key={i}
-              path={path}
-              element={
-                <Decoder />
-              }
-            />
-          ))}
+          <Route path="/jwt-decoder" element={<Decoder />} />
+          <Route path="/home" element={<Decoder />} />
           <Route path="/jwt-encoder" element={<Encoder />} />
+          <Route path="*" element={<Navigate to="/jwt-decoder" />} />
         </Routes>
-      </BrowserRouter>
-    </>
+      </RedirectWrapper>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "generate-redirects",
-      closeBundle() {
-        fs.writeFileSync("dist/_redirects", "/* /index.html 200");
-      },
-    },
-  ],
+  plugins: [react()],
 });
